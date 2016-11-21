1651494acbb34e7c40eb66776c9e5816a8f51301
László Szabó <lszabo@opswat.com>
2016-11-21 21:17:20 +0100

    added tutorial_003

---
+++ src/tutorial_003/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include <QCoreApplication>
+
+#include <SDL.h>
+
+#include <exception.h>
+
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
+
+SDL_Window *window = nullptr;
+SDL_Surface *windowSurface = nullptr;
+SDL_Surface *imageSurface = nullptr;
+
+void init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        throw Exception(QStringLiteral("Could not initialize SDL"));
+    }
+
+    window = SDL_CreateWindow("Tutorial 003", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        throw Exception(QStringLiteral("Could not create window"));
+    }
+
+    windowSurface = SDL_GetWindowSurface(window);
+}
+
+void loadMedia()
+{
+    imageSurface = SDL_LoadBMP(qApp->applicationDirPath().append("/x_out_the_window.bmp").toStdString().c_str());
+
+    if (!imageSurface) {
+        throw Exception(QStringLiteral("Could not load bmp image"));
+    }
+}
+
+void close()
+{
+    SDL_FreeSurface(imageSurface);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
+    Q_UNUSED(app);
+    
+    try{
+        init();
+        loadMedia();
+    } catch (const Exception &exception) {
+        std::cerr << "Error: " << exception.stdMsg() << " " << SDL_GetError() << std::endl;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_BlitSurface(imageSurface, nullptr, windowSurface, nullptr);
+        SDL_UpdateWindowSurface(window);
+    }
+
+    close();
+
+    return 0;
+}
