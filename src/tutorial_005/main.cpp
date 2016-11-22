920be4d96d5e36213556c1037672c2c7c5626578
László Szabó <lszabo@opswat.com>
2016-11-22 12:19:30 +0100

    added tutorial_005

---
+++ src/tutorial_005/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+#include <QCoreApplication>
+
+#include <SDL.h>
+
+#include <exception.h>
+
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
+
+SDL_Window *window = nullptr;
+SDL_Surface *windowSurface = nullptr;
+SDL_Surface *imageSurface = nullptr;
+
+SDL_Surface *loadSurface(const QString &path)
+{
+    SDL_Surface *loaded = SDL_LoadBMP(path.toStdString().c_str());
+
+    if (!loaded) {
+        throw Exception(QStringLiteral("Error loading bmp: ").append(path));
+    }
+
+    SDL_Surface *optimized = SDL_ConvertSurface(loaded, windowSurface->format, 0);
+    SDL_FreeSurface(loaded);
+
+    return optimized;
+}
+
+void init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        throw Exception(QStringLiteral("Could not initialize SDL"));
+    }
+
+    window = SDL_CreateWindow("Tutorial 005", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        throw Exception(QStringLiteral("Could not create window"));
+    }
+
+    windowSurface = SDL_GetWindowSurface(window);
+}
+
+void loadMedia()
+{
+    imageSurface = loadSurface(qApp->applicationDirPath().append(QStringLiteral("/stretch.bmp")));
+}
+
+void close()
+{
+    SDL_FreeSurface(imageSurface);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
+    Q_UNUSED(app);
+    
+    try{
+        init();
+        loadMedia();
+    } catch (const Exception &exception) {
+        std::cerr << "Error: " << exception.stdMsg() << " " << SDL_GetError() << std::endl;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    SDL_Rect stretchRect = {
+        0,
+        0,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT
+    };
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_BlitScaled(imageSurface, nullptr, windowSurface, &stretchRect);
+        SDL_UpdateWindowSurface(window);
+    }
+
+    close();
+
+    return 0;
+}
